b2e5b6e8eca2de2be33e198750354aff41df9e63
Senya-P <senyapsent@gmail.com>
2025-03-06 22:44:59 +0100

    install + list commands

--- src/main/java/cz/cuni/mff/releasemanager/ReleaseManager.java
+++ src/main/java/cz/cuni/mff/releasemanager/ReleaseManager.java
@@ -1,5 +1,10 @@
 package cz.cuni.mff.releasemanager;
 
+import java.io.IOException;
+import java.util.List;
+
+import cz.cuni.mff.releasemanager.types.ReleaseInfo;
+import cz.cuni.mff.releasemanager.types.ReleasesList;
 import cz.cuni.mff.releasemanager.types.Repo;
 import cz.cuni.mff.releasemanager.types.SearchResult;
 
@@ -9,12 +14,12 @@ public class ReleaseManager {
 
     private final CmdParser cmdParser;
     private final GithubClient githubClient;
-    private final Platform platform;
+    private final PlatformHandler platformHandler;
 
     private ReleaseManager() {
-        platform = Platform.detectPlatform();
         cmdParser = new CmdParser();
         githubClient = new GithubClient();
+        platformHandler = Platform.getPlatformHandler(); // two instances! -> singleton?
     }
 
     public static ReleaseManager getInstance() {
@@ -39,21 +44,23 @@ public class ReleaseManager {
     private void search(Command command) {
         // search for the release
         SearchResult searchResult = githubClient.searchRepoByName(command.argument);
-        if (searchResult == null) {
+        if (searchResult == null || searchResult.items().isEmpty()) {
             System.out.println("No results found.");
             return;
         }
         System.out.println("Found " + searchResult.items().size() + " repositories.");
         for (Repo repo : searchResult.items()) {
-            String owner = repo.fullName().split("/")[0];
-            String repoName = repo.fullName().split("/")[1];
-            System.out.println(repoName + ": " + repo.description());
+            System.out.println(repo.fullName() + ": " + repo.description());
         }
         System.out.println("To install a release, use the 'install' command followed by the repository name.");
     }
 
     private void install(Command command) {
-        if (githubClient.getLatestRelease("", "")) {
+        if (command.argument.split("/").length != 2) {
+            System.out.println("Please specify the correct repository name of format 'owner/repo'.");
+            return;
+        }
+        if (githubClient.getLatestRelease(command.argument)) {
             System.out.println("Installation successful.");
         } else {
             System.out.println("Installation failed.");
@@ -65,7 +72,19 @@ public class ReleaseManager {
     }
 
     private void list() {
-        // list the releases
+        try {
+            ReleasesList releasesList = platformHandler.loadReleasesList();
+            if (releasesList == null) {
+                System.out.println("No releases installed.");
+                return;
+            }
+            List<ReleaseInfo> releases = releasesList.releases();
+            for (ReleaseInfo release : releases) {
+                System.out.println(release.repo());
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to load releases list.");
+        }
     }
 
     private void help() {
