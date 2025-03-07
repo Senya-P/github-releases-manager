7b71dfe028efdd56a2c356807bdb9d5890e65067
Senya-P <senyapsent@gmail.com>
2025-03-07 20:29:04 +0100

    command parsing rewrited

--- src/main/java/cz/cuni/mff/releasemanager/CmdParser.java
+++ src/main/java/cz/cuni/mff/releasemanager/CmdParser.java
@@ -1,5 +1,7 @@
 package cz.cuni.mff.releasemanager;
 
+import java.util.Arrays;
+
 
 public class CmdParser {
 
@@ -7,34 +9,14 @@ public class CmdParser {
     }
 
     public Command parse(String[] args) {
-        if (args.length == 1) {
-            if (args[0].equals("list")) {
-                return Command.LIST;
-            }
-            else if (args[0].equals("help")) {
-                return Command.HELP;
-            }
-            else return Command.HELP;
+        Command defaultCommand = Command.HELP;
+        defaultCommand.argument = Arrays.toString(args);
+
+        Command command = Command.fromName(args[0]).orElseGet(() -> defaultCommand);
+        if (command.requiresArgument()) {
+            command.argument = args[1];
         }
-        else if (args.length == 2) {
-            if (args[0].equals("search")) {
-                Command command = Command.SEARCH;
-                command.argument = args[1];
-                return command;
-            }
-            else if (args[0].equals("install")) {
-                Command command = Command.INSTALL;
-                command.argument = args[1];
-                return command;
-            }
-            else if (args[0].equals("update")) {
-                Command command = Command.UPDATE;
-                command.argument = args[1];
-                return command;
-            }
-            else return Command.HELP;
-        }
-        return Command.HELP;
+        return command;
     }
 
 }
--- src/main/java/cz/cuni/mff/releasemanager/Command.java
+++ src/main/java/cz/cuni/mff/releasemanager/Command.java
@@ -1,15 +1,34 @@
 package cz.cuni.mff.releasemanager;
 
-public enum Command { //iface?
-    SEARCH("search"),
-    INSTALL("install"),
-    UPDATE("update"),
-    LIST("list"),
-    HELP("help");
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-    Command(String command) {
+public enum Command {
+    SEARCH("search", 0),
+    INSTALL("install", 1),
+    UPDATE("update", 1),
+    LIST("list", 0),
+    HELP("help", 0);
+
+    Command(String command, int argNumber) {
         this.commandName = command;
+        this.argNumber = argNumber;
     }
     public final String commandName;
     public String argument;
+    public int argNumber;
+
+    private static final Map<String, Command> COMMAND_MAP = Arrays.stream(values())
+        .collect(Collectors.toMap(c -> c.commandName, Function.identity()));
+
+    public static Optional<Command> fromName(String name) {
+        return Optional.ofNullable(COMMAND_MAP.get(name.toLowerCase()));
+    }
+
+    public boolean requiresArgument() {
+        return argNumber == 1;
+    }
 }
