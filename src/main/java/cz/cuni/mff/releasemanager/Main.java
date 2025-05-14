efd7b07d5400653d1070ba9b5863d685963add4a
Senya-P <senyapsent@gmail.com>
2025-05-14 20:00:54 +0200

    main

--- src/main/java/cz/cuni/mff/releasemanager/Main.java
+++ src/main/java/cz/cuni/mff/releasemanager/Main.java
@@ -2,14 +2,12 @@ package cz.cuni.mff.releasemanager;
 
 public class Main { // extends Runnable
     public static void main(String[] args) {
-        // ReleaseManager releaseManager = ReleaseManager.getInstance();
-        // releaseManager.execute(args);
-
-        GithubClient githubClient = new GithubClient();
-        githubClient.getLatestRelease("keepassxreboot", "keepassxc");
-
-        // LinuxHandler lh = new LinuxHandler();
-        // lh.extract(Paths.get("/home/senya/Documents/code/github-releases-manager/releases/keepassxc-2.7.9-src.tar.xz"), Paths.get("/home/senya/Documents/code/github-releases-manager/releases/dest"));
+        ReleaseManager releaseManager = ReleaseManager.getInstance();
+        //String[] cmd = {"list"};
+        //String[] cmd = {"install", "keepassxreboot/keepassxc"};
+        //String[] cmd = {"search", "keepass"};
+        releaseManager.execute(args);
+        //releaseManager.list();
     }
 }
 
@@ -18,5 +16,6 @@ public class Main { // extends Runnable
  * multi-thread
  * version control: json?
  * resource bundle
- * 
+ * filter unsupported repos
+ * no internet conn
  */
