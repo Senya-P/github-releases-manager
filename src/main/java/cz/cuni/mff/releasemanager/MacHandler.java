83443ed5e2535f8becb51d8cec025e2522c04b29
Senya-P <senyapsent@gmail.com>
2025-05-15 19:19:20 +0200

    MacOS impl

--- src/main/java/cz/cuni/mff/releasemanager/MacHandler.java
+++ src/main/java/cz/cuni/mff/releasemanager/MacHandler.java
@@ -1,10 +1,17 @@
 package cz.cuni.mff.releasemanager;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MacHandler extends PlatformHandler {
 
     private static MacHandler instance;
+    private static final Path MOUNT_DIR = Paths.get(
+        System.getProperty("user.home"),
+        "Volumes"
+    );
 
     private MacHandler() {}
 
@@ -17,11 +24,76 @@ public class MacHandler extends PlatformHandler {
 
     @Override
     public Path install(Path asset) {
-        return null;
+        try {
+            Process process = new ProcessBuilder()
+                .command("hdiutil", "attach", "-nobrowse", asset.toString())
+                .inheritIO()
+                .start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new IOException("Installation failed with exit code: " + exitCode);
+            }
+            Path appName = getShortCut(asset);
+            Path targetDir = Paths.get(System.getProperty("user.home"), "Applications");
+
+            Path app = findAppInMountDir(appName);
+
+            Process copyProcess = new ProcessBuilder()
+                .command("cp", "-rf", app.toString(), targetDir.toString())
+                .start();
+            if (copyProcess.waitFor() != 0) {
+                throw new IOException("Failed to copy application.");
+            }
+            detach(app);
+            return targetDir.resolve(appName);
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Installation error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private Path findAppInMountDir(Path appName) throws IOException {
+        return Files.walk(MOUNT_DIR)
+            .filter(path -> {
+                String fileName = path.getFileName().toString();
+                if (!Files.isDirectory(path)) {
+                    return fileName.equalsIgnoreCase(appName.toString());
+                }
+                return fileName.substring(0, fileName.length() - 4).equalsIgnoreCase(appName.toString());
+            })
+            .findFirst()
+            .orElseThrow(() -> new IOException("Error finding app."));
+    }
+
+    private void detach(Path mountPath) throws IOException, InterruptedException {
+        Process process = new ProcessBuilder()
+                .command("hdiutil", "detach", mountPath.toString())
+                .inheritIO()
+                .start();
+
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new IOException("Installation failed with exit code: " + exitCode);
+        }
     }
 
     @Override
     public void uninstall(Path asset) {
+        try {
+            Process process = new ProcessBuilder()
+                .command("rm", "-rf", asset.toString())
+                .start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new IOException("Uninstallation failed with exit code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Uninstallation error: " + e.getMessage());
+        }
     }
 
     @Override
@@ -31,12 +103,22 @@ public class MacHandler extends PlatformHandler {
 
     @Override
     void createReleasesListFile() {
-        throw new UnsupportedOperationException("Unimplemented method 'createConfig'");
+        Path releasesFile = getReleasesListFileLocation();
+        Path releasesDir = getReleasesListDirLocation();
+        if (Files.exists(releasesFile)) {
+            return;
+        }
+        try {
+            Files.createDirectories(releasesDir);
+            Files.createFile(releasesFile);
+        } catch (IOException e) {
+            System.out.println("Failed to create : " + e.getMessage());
+        }
     }
 
     @Override
     protected Path getReleasesListDirLocation() {
-        throw new UnsupportedOperationException("Unimplemented method 'getReleasesListLocation'");
+        return Paths.get(System.getProperty("user.home"), "Library", "Application Support", APP_DATA_DIR);
     }
 
 }
