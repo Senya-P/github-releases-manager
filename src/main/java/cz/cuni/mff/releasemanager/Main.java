a0ed03ba9f1d3749d85753e2d4abbd76ba93fa5e
Senya-P <senyapsent@gmail.com>
2025-05-19 21:27:02 +0200

    clean main

--- src/main/java/cz/cuni/mff/releasemanager/Main.java
+++ src/main/java/cz/cuni/mff/releasemanager/Main.java
@@ -1,21 +1,8 @@
 package cz.cuni.mff.releasemanager;
 
-public class Main { // extends Runnable
+public class Main {
     public static void main(String[] args) {
         ReleaseManager releaseManager = ReleaseManager.getInstance();
-        //String[] cmd = {"list"};
-        //String[] cmd = {"install", "keepassxreboot/keepassxc"};
-        //String[] cmd = {"search", "keepass"};
         releaseManager.execute(args);
-        //releaseManager.list();
     }
 }
-
-/*
- * blocking cmd iface?
- * multi-thread
- * version control: json?
- * resource bundle
- * filter unsupported repos
- * no internet conn
- */
