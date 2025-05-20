50c525aed2289a477323e7335f581dcb2070c3d0
Senya-P <senyapsent@gmail.com>
2025-05-20 23:07:24 +0200

    adjust version

--- src/main/java/cz/cuni/mff/releasemanager/package-info.java
+++ src/main/java/cz/cuni/mff/releasemanager/package-info.java
@@ -2,6 +2,7 @@
  * Provides core functionality for managing GitHub releases,
  * including installation, update, search, and uninstallation
  * of release assets through a command-line interface.
- *  @author Kseniia Popova
+ * @author Kseniia Popova
+ * @version 1.0
  */
 package cz.cuni.mff.releasemanager;
